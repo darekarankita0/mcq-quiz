2ca519cfda0f7d6c71875970c11c4a18fcf1309b
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2021-06-26 00:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,6 +168,8 @@ def check_answer():
 if __name__ == '__main__':
 	with app.test_request_context("/"):
 		session["key"] = "value"
+	app.listen(process.env.PORT || 3000, function(){console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 	#app.secret_key = 'super secret key'
 	#app.config['SESSION_TYPE'] = 'filesystem'
