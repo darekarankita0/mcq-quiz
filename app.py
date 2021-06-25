ab9242f8f407d683e0972d98abedfb402d7cf76a
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2021-06-26 01:06:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -171,8 +171,8 @@ if __name__ == '__main__':
 	#const PORT = process.env.PORT || '8080'
 	#app=express();
 	#app.set("port",PORT);
-	host="0.0.0.0"
-	port=33507
+	#host="0.0.0.0"
+	#port=33507
 	app.run()
 	#app.secret_key = 'super secret key'
 	#app.config['SESSION_TYPE'] = 'filesystem'
