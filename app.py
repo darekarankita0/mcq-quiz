875849f37614c734c3ed578e0e135dabcefebaa2
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2021-06-25 23:28:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from copy import deepcopy
 from datetime import datetime, date, time
 from random import choice, shuffle
 # Create a flask app and set its secret key
-app = Flask(__name__)
+#app = Flask(__name__)
 #app.secret_key = os.urandom(24)
 #
+app = Flask(__name__)
+SESSION_TYPE = "redis"
+PERMANENT_SESSION_LIFETIME = 1800
+
+app.config.update(SECRET_KEY=os.urandom(24))
+
+app.config.from_object(__name__)
+Session(app)
 questions = {
 '1': {'tip': 'Proximity Detection', 'answer': 'Proximity Detection','question': 'What is the use of the Ultrasonic Sensor?','options': ['Proximity Detection','Humidity Detection','Image Processing','GPS']},'4': {'tip': '4', 'answer': '4', 'question': "How many pins are present in the Ultrasonic Sensor",'options': ['1', '3', '2', '4']}, '2': {'tip': 'Digital', 'answer': 'Digital', 'question': 'What mode should we put the Arduino pin to, in order for object detection to work with the Ultrasonic Sensor?','options': ['Analog','Digital','PCM','TDM']},'6': {'tip': 'Damage is caused', 'answer': 'Damage is caused','question': 'What will happen if we supply a voltage of 25V to the Vcc of the Ultrasonic sensor?','options': ['Damage is caused','Sensor will work fine','Sensor will not respond for the time the voltage is applied','Sensor will function normally']}, '5': {'tip': 'Object is oscillating side by side', 'answer': "Object is oscillating side by side", 'question': 'If 1 means an object is detected and 0 meaning no object is detected, then considering the sensor is stationary, what can be said about the movement of the object if the output by the sensor is 1010101?', 'options': ['Object is stationary','Object is oscillating side by side','Object is continuously moving away','Object is continuously moving closer']},'3': {'tip': 'Sound', 'answer': 'Sound', 'question': 'What kind of waves does the Ultrasonic Sensor work on?', 'options': ['Gas','Heat','Light','Sound']},'7': {'tip': '15 mA', 'answer': '15 mA', 'question': 'What is the optimum current that is required to operate the Ultrasonic Sensor?', 'options': ['20 mA','15 mA','200 A','1 A']}
 }
@@ -157,10 +165,12 @@ def check_answer():
     return render_template('python_answer.html',num=currentN,ntot=app.nquestions,descript=tip,anscheck1=the_check1,anscheck2=the_check2,anscheck3=the_check3,anscheck4=the_check4,ans_color1=the_color1,ans_color2=the_color2,ans_color3=the_color3,ans_color4=the_color4,ans_color6=the_color6,question=currentQ,ans1=a1,ans2=a2,ans3=a3,ans4=a4)
 # Runs the app using the web server on port 80, the standard HTTP port
 if __name__ == '__main__':
-	app.secret_key = 'super secret key'
-	app.config['SESSION_TYPE'] = 'filesystem'
-	sess.init_app(app)
-	app.debug=True
+	    with app.test_request_context("/"):
+			session["key"] = "value"
+	#app.secret_key = 'super secret key'
+	#app.config['SESSION_TYPE'] = 'filesystem'
+	#sess.init_app(app)
+	#app.debug=True
 	app.run( 
         # host="0.0.0.0",
         # port=33507
